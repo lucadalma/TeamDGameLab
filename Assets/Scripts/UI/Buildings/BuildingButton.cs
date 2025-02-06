71b7cdc9ed36ea829b0896273a69d47aecfaa878
Davide Danese <Daviderun2@gmail.com>
2025-02-06 18:22:18 +0100

    ui building e loro spawn

---
+++ Assets/Scripts/UI/Buildings/BuildingButton.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingButton : MonoBehaviour
+{
+    [NonSerialized] public Vector2 centerPoint;
+
+    public GameObject BuildingTimer;
+    public enum TypeEnum
+    {
+        category,
+        building,
+        back
+    }
+
+    public enum CtegoryEnum
+    {
+        nullCategory,
+        units,
+        upgrades,
+        abilities,
+        fortification,
+        supplayDepot,
+
+    }
+
+    UIManager uIM;
+
+    public TypeEnum type;
+    public CtegoryEnum category;
+
+    void Start()
+    {
+        uIM = FindObjectOfType<UIManager>();
+    }
+
+    public void openCategory()
+    {
+        if (TypeEnum.category == type)
+            uIM.selectBuildingCategory(centerPoint, category);
+    }
+
+    public void backToCategorySelector()
+    {
+        if (TypeEnum.back == type)
+        {
+            uIM.removeBuiidingMenu();
+            uIM.openBuildingMenu(transform.position);
+        }
+    }
+
+    public void StartBuildingTimer()
+    {
+        uIM.addBuildingOnTimer(BuildingTimer);
+    }
+}
--- Assets/Scripts/UI/Troupes/UIManager.cs
+++ Assets/Scripts/UI/Troupes/UIManager.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     int posiblePages;
     int currentPage = 0;
+
+    [SerializeField] GameObject staticUI;
 
     [NonSerialized] public int numberOfUnitsOnTimer;
     [NonSerialized] public int numberOfUnitsOnDeploy;
@@ -21,9 +28,21 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] List<Transform> creationSelectorSlots;
     [SerializeField] List<Transform> deploimentSelectorSlots;
-    [SerializeField] List<Transform> timerSlots;
+    [SerializeField] List<Transform> unitTimerSlots;
+    [SerializeField] List<Transform> buildingTimerSlots;
 
+    [SerializeField] List<GameObject> availableBuildingCategories;
+    [SerializeField] List<GameObject> availableBuildings;
+    [SerializeField] GameObject BuildingBackButton;
 
+    List<GameObject> buildingButtonsDysplayed = new List<GameObject>();
+    List<GameObject> buildingsOnTimer = new List<GameObject>();
+
+    [NonSerialized] public GameObject TargetBuilding;
+    [SerializeField] private LayerMask buildingLayerMask;
+    [SerializeField] float r;
+
+    [NonSerialized] public int numberOfBuildingsOnTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,15 +56,13 @@ public class UIManager : MonoBehaviour
         }
 
 
-
-
         SetAvailableUnits();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        searchForBuilding();
     }
 
 
@@ -130,8 +147,8 @@ public class UIManager : MonoBehaviour
         {
             for (int i = 0; i < unitsOnTimer.Count; i++)
             {
-                unitsOnTimer[i].transform.SetParent(timerSlots[i]);
-                unitsOnTimer[i].transform.position = timerSlots[i].position;
+                unitsOnTimer[i].transform.SetParent(unitTimerSlots[i]);
+                unitsOnTimer[i].transform.position = unitTimerSlots[i].position;
                 if (i == 0)
                 {
                     unitsOnTimer[i].GetComponent<CreationTimer>().first = true;
@@ -149,7 +166,7 @@ public class UIManager : MonoBehaviour
         {
             Debug.Log("now on timer" + unit.name);
 
-            GameObject newUnit = Instantiate(unit, timerSlots[0]);
+            GameObject newUnit = Instantiate(unit, unitTimerSlots[0]);
 
             unitsOnTimer.Add(newUnit);
             setUnitOnTimer();
@@ -164,7 +181,7 @@ public class UIManager : MonoBehaviour
     }
 
 
-    public void remopveUnitOnTimer(GameObject unit)
+    public void removeUnitOnTimer(GameObject unit)
     {
         Debug.Log("removed" + unit.name);
         unitsOnTimer.Remove(unit);
@@ -202,7 +219,7 @@ public class UIManager : MonoBehaviour
     void setDeployUnits()
     {
 
-       availableUnitsToDeploy = availableUnitsToDeploy.OrderBy(item => item.GetComponent<unitDeployButton>().unitID).ToList();
+        availableUnitsToDeploy = availableUnitsToDeploy.OrderBy(item => item.GetComponent<unitDeployButton>().unitID).ToList();
 
         for (int i = 0; i < availableUnitsToDeploy.Count; i++)
         {
@@ -210,5 +227,225 @@ public class UIManager : MonoBehaviour
             availableUnitsToDeploy[i].transform.position = deploimentSelectorSlots[i].position;
         }
         numberOfUnitsOnDeploy = availableUnitsToDeploy.Count;
+    }
+
+
+    // Fuctions to disable or enable the static ui
+    void turnOffInteractionsWithStaticUI(Transform parent)
+    {
+
+        foreach (Transform child in parent)
+        {
+
+            Image image = child.GetComponent<Image>();
+            Button button = child.GetComponent<Button>();
+
+
+            if (image != null)
+            {
+
+                image.raycastTarget = false;
+
+            }
+
+            if (button != null)
+            {
+                button.enabled = false;
+            }
+
+
+            if (child.childCount > 0)
+            {
+                turnOffInteractionsWithStaticUI(child);
+            }
+        }
+
+    }
+
+    void turnOnInteractionsWithStaticUI(Transform parent)
+    {
+
+        foreach (Transform child in parent)
+        {
+
+            Image image = child.GetComponent<Image>();
+            Button button = child.GetComponent<Button>();
+
+
+            if (image != null)
+            {
+
+                image.raycastTarget = true;
+
+            }
+
+            if (button != null)
+            {
+                button.enabled = true ;
+            }
+
+            if (child.childCount > 0)
+            {
+                turnOnInteractionsWithStaticUI(child);
+            }
+        }
+
+    }
+
+
+
+    //buildin buttons functions
+    void searchForBuilding()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (TargetBuilding == null)
+                {
+                    Debug.Log("sheatch build");
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, buildingLayerMask))
+                    {
+                        turnOffInteractionsWithStaticUI(staticUI.transform);
+                        Debug.Log("build found");
+                        TargetBuilding = hit.collider.gameObject;
+                        openBuildingMenu(Input.mousePosition);
+                    }
+                }
+                else
+                {
+                    TargetBuilding = null;
+                    removeBuiidingMenu();
+                    turnOnInteractionsWithStaticUI(staticUI.transform);
+                }
+            }
+        }
+    }
+
+    public void openBuildingMenu(Vector2 CenterPoint)
+    {
+        Vector2 spawnPoint;
+        float numberOfCategories = availableBuildingCategories.Count;
+        float ang = (360f / numberOfCategories) * Mathf.Deg2Rad;
+
+        for (int i = 0; i < availableBuildingCategories.Count; i++)
+        {
+            spawnPoint = new Vector2(r * Mathf.Sin(ang * i) + CenterPoint.x, r * Mathf.Cos(ang * i) + CenterPoint.y);
+            GameObject temp = Instantiate(availableBuildingCategories[i], spawnPoint, transform.rotation, transform);
+            temp.GetComponent<BuildingButton>().centerPoint = CenterPoint;
+            buildingButtonsDysplayed.Add(temp);
+        }
+    }
+    public void removeBuiidingMenu()
+    {
+        foreach (var buildingButton in buildingButtonsDysplayed)
+        {
+            Destroy(buildingButton.gameObject);
+        }
+
+        buildingButtonsDysplayed.Clear();
+
+        
+    }
+
+
+    public void selectBuildingCategory(Vector2 CenterPoint, BuildingButton.CtegoryEnum Category)
+    {
+        foreach (var buildingButton in buildingButtonsDysplayed)
+        {
+            Destroy(buildingButton.gameObject);
+        }
+        buildingButtonsDysplayed.Clear();
+
+        GameObject backButton =Instantiate(BuildingBackButton, CenterPoint, transform.rotation, transform);
+        buildingButtonsDysplayed.Add(backButton);
+
+        List<GameObject> buttonsInCurrentCategory = new List<GameObject>();
+        buttonsInCurrentCategory.Clear();
+
+        foreach (var buildingButton in availableBuildings)
+        {
+            if(buildingButton.GetComponent<BuildingButton>().category == Category)
+            {
+                buttonsInCurrentCategory.Add(buildingButton);
+            }
+        }
+
+        Vector2 spawnPoint;
+        float numberOfCategories = buttonsInCurrentCategory.Count;
+        float ang = (360f / numberOfCategories) * Mathf.Deg2Rad;
+
+        for (int i = 0; i < buttonsInCurrentCategory.Count; i++)
+        {
+            spawnPoint = new Vector2(r * Mathf.Sin(ang * i) + CenterPoint.x, r * Mathf.Cos(ang * i) + CenterPoint.y);
+            GameObject temp = Instantiate(buttonsInCurrentCategory[i], spawnPoint, transform.rotation, transform);
+            temp.GetComponent<BuildingButton>().centerPoint = spawnPoint;
+            buildingButtonsDysplayed.Add(temp);
+        }
+
+    }
+
+
+
+    public void setBuildingOnTimer()
+    {
+        if (buildingsOnTimer.Count > 0)
+        {
+            for (int i = 0; i < buildingsOnTimer.Count; i++)
+            {
+                buildingsOnTimer[i].transform.SetParent(buildingTimerSlots[i]);
+                buildingsOnTimer[i].transform.position = buildingTimerSlots[i].position;
+                if (i == 0)
+                {
+                    buildingsOnTimer[i].GetComponent<CreationTimer>().first = true;
+                }
+            }
+        }
+
+        numberOfBuildingsOnTimer = buildingsOnTimer.Count;
+
+    }
+
+    public void addBuildingOnTimer(GameObject building)
+    {
+        if (building != null) 
+        {
+            if (numberOfBuildingsOnTimer < 4)
+            {
+                Debug.Log("now on timer" + building.name);
+
+                GameObject newBuilding = Instantiate(building, buildingTimerSlots[0]);
+                newBuilding.GetComponent<CreationTimer>().buildingSlot = TargetBuilding;
+
+                buildingsOnTimer.Add(newBuilding);
+                setBuildingOnTimer();
+                removeBuiidingMenu();
+                turnOnInteractionsWithStaticUI(staticUI.transform);
+            }
+            else
+            {
+                Debug.LogWarning("no space for building to be created");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("building does not have setup");
+        }
+
+
+
+
+    }
+
+
+    public void removeBuildingOnTimer(GameObject building)
+    {
+        Debug.Log("removed" + building.name);
+        buildingsOnTimer.Remove(building);
+        Destroy(building);
+        setBuildingOnTimer();
     }
 }
