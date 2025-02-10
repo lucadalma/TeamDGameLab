54992ceddcd896abdf9e0798818081c713284f35
Luca Dal Maso <lucadalmaso2001@gmail.com>
2025-02-10 13:59:39 +0100

    Base movement

--- Assets/Scripts/Wave/BaseScript.cs
+++ Assets/Scripts/Wave/BaseScript.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseScript : MonoBehaviour
 {
     [SerializeField]
     GameEvent evento;
+
+
+    public Slider speedSlider;
+    public int moveSpeed;
+    private float speed;
+
+    void Update()
+    {
+        if (speedSlider != null)
+        {
+            if (speedSlider.value > 0 && speedSlider.value < 0.9f)
+            {
+                speedSlider.value = 0;
+            }
+            else if(speedSlider.value < 0 && speedSlider.value > -0.9f)
+            {
+                speedSlider.value = 0;
+            }
+            speed = speedSlider.value;
+            transform.Translate(Vector3.forward * -(speed * moveSpeed) * Time.deltaTime);
+
+        }
+    }
 
     private void OnDestroy()
     {
