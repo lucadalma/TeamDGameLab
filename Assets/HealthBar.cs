14fe5348fda3d3a28b6cddc87693255c2e6e406e
Def0Coder <fmdef079@gmail.com>
2025-02-17 16:45:22 +0100

    HealthBar rotation + enemt swarm HB modify

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -23,17 +23,22 @@ public class HealthBar : MonoBehaviour
 
     public void Update()
     {
-        if(healthSlider.value != health)
+        // Mantiene il valore della health aggiornata sulla barra
+        if (healthSlider.value != health)
         {
             healthSlider.value = health;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Rotazione per far sempre guardare la healthbar verso la camera
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0); // Inverte la rotazione per essere correttamente visibile
+
+        // Test: Premi Spazio per togliere 10 HP
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(10);
-
         }
-       
+
     }
 
     public void TakeDamage(float damage)
