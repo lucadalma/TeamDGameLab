addb5ea7309afbcf89f168f5167501d970293860
Luca Dal Maso <lucadalmaso2001@gmail.com>
2025-06-04 09:47:01 +0200

    Fix

--- Assets/Scripts/EnemyBaseBuildings/EnemyBase_BuildingManager.cs
+++ Assets/Scripts/EnemyBaseBuildings/EnemyBase_BuildingManager.cs
@@ -6,6 +6,8 @@ public class EnemyBase_BuildingManager : MonoBehaviour
 {
     public List<GameObject> buildings;
 
+    public BoolVariable pause;
+
     public SO_EnemyBuilding enemyBuildingsList;
     private void Start()
     {
@@ -19,17 +21,26 @@ public class EnemyBase_BuildingManager : MonoBehaviour
 
             for (int i = 0; i < enemyBuildingsList.buildings.Count; i++)
             {
+                while (pause.Value) 
+                {
+                    yield return null;
+                }
                 yield return new WaitForSeconds(enemyBuildingsList.timers[i]);
+
                 if (buildings[i] != null)
                 {
                     GameObject building = Instantiate(enemyBuildingsList.buildings[i], buildings[i].transform.position, Quaternion.identity, buildings[i].gameObject.transform.parent);
                     building.GetComponent<EnemyBase_Event>().unitToUpgrade = enemyBuildingsList.units[i];
                     GameObject build = buildings[i];
                     //buildings.Remove(buildings[i]);
+                    buildings[i] = building;
                     Destroy(build);
+
 
                     //enemyBuildingsList.buildings.Remove(enemyBuildingsList.buildings[i])
                 }
+
+
             }
         }
     }
